e3d87151bb4c32ddf0a1bba07265e913e5dcf9b9
Schlatter076 <schlatter076@163.com>
2019-11-03 18:39:53 +0800

    第一个springMVC项目

---
+++ src/main/java/org/loyer/web/controller/MyController.java
@@ -0,0 +1,17 @@
+package org.loyer.web.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "欢迎来到Loyer的Web世界");
+        mv.setViewName("/WEB-INF/jsp/welcome.jsp");
+        return mv;
+    }
+}
